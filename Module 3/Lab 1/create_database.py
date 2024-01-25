2e1809d16bd9a6464a384ffd439f2c2f99e9b1e9
Haoran <a@h88i.com>
2024-01-25 12:05:16 -0500

    Create create_database.py
    创建create_database测试文件

---
+++ Module 3/Lab 1/create_database.py
@@ -0,0 +1,19 @@
+import snowflake.connector
+import os
+
+# Gets the version
+ctx = snowflake.connector.connect(
+    user= os.getenv('SNOWFLAKE_USER'),
+    password= os.getenv('SNOWFLAKE_PASS'),
+    account=os.getenv('SNOWFLAKE_ACCT'),
+    role='ACCOUNTADMIN'
+    )
+cs = ctx.cursor()
+try:
+ conn.cursor().execute("CREATE WAREHOUSE IF NOT EXISTS tiny_warehouse_mg")
+ conn.cursor().execute("CREATE DATABASE IF NOT EXISTS testdb_mg")
+ conn.cursor().execute("USE DATABASE testdb_mg")
+ conn.cursor().execute("CREATE SCHEMA IF NOT EXISTS testschema_mg")
+finally:
+    cs.close()
+ctx.close()
